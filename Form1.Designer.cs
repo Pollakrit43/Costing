5df25f40cd4cad97718b84403bc8a96368dbfc7d
Pollakrit43 <s6252410020@sau.ac.th>
2024-02-25 13:45:47 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Costing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dgvColumns = new System.Windows.Forms.DataGridView();
+            this.Style = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Customer = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.F1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.F2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.F3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.F4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.F5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.F6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.F7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.F8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.F9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.F10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.F11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.F12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.cboStyle = new System.Windows.Forms.ComboBox();
+            this.cboCustomer = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvColumns)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvColumns
+            // 
+            this.dgvColumns.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvColumns.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvColumns.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Style,
+            this.Customer,
+            this.F1,
+            this.F2,
+            this.F3,
+            this.F4,
+            this.F5,
+            this.F6,
+            this.F7,
+            this.F8,
+            this.F9,
+            this.F10,
+            this.F11,
+            this.F12});
+            this.dgvColumns.Location = new System.Drawing.Point(12, 70);
+            this.dgvColumns.Name = "dgvColumns";
+            this.dgvColumns.Size = new System.Drawing.Size(776, 267);
+            this.dgvColumns.TabIndex = 0;
+            // 
+            // Style
+            // 
+            this.Style.HeaderText = "Style";
+            this.Style.Name = "Style";
+            this.Style.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Customer
+            // 
+            this.Customer.HeaderText = "Customer";
+            this.Customer.Name = "Customer";
+            this.Customer.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // F1
+            // 
+            this.F1.HeaderText = "F1";
+            this.F1.Name = "F1";
+            this.F1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // F2
+            // 
+            this.F2.HeaderText = "F2";
+            this.F2.Name = "F2";
+            this.F2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // F3
+            // 
+            this.F3.HeaderText = "F3";
+            this.F3.Name = "F3";
+            this.F3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // F4
+            // 
+            this.F4.HeaderText = "F4";
+            this.F4.Name = "F4";
+            this.F4.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // F5
+            // 
+            this.F5.HeaderText = "F5";
+            this.F5.Name = "F5";
+            this.F5.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // F6
+            // 
+            this.F6.HeaderText = "F6";
+            this.F6.Name = "F6";
+            this.F6.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // F7
+            // 
+            this.F7.HeaderText = "F7";
+            this.F7.Name = "F7";
+            this.F7.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // F8
+            // 
+            this.F8.HeaderText = "F8";
+            this.F8.Name = "F8";
+            this.F8.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // F9
+            // 
+            this.F9.HeaderText = "F9";
+            this.F9.Name = "F9";
+            this.F9.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // F10
+            // 
+            this.F10.HeaderText = "F10";
+            this.F10.Name = "F10";
+            this.F10.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // F11
+            // 
+            this.F11.HeaderText = "F11";
+            this.F11.Name = "F11";
+            this.F11.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // F12
+            // 
+            this.F12.HeaderText = "F12";
+            this.F12.Name = "F12";
+            this.F12.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(321, 30);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(669, 28);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // cboStyle
+            // 
+            this.cboStyle.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.cboStyle.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.cboStyle.FormattingEnabled = true;
+            this.cboStyle.Location = new System.Drawing.Point(13, 30);
+            this.cboStyle.Name = "cboStyle";
+            this.cboStyle.Size = new System.Drawing.Size(121, 21);
+            this.cboStyle.TabIndex = 3;
+            this.cboStyle.SelectedIndexChanged += new System.EventHandler(this.cboStyle_SelectedIndexChanged);
+            // 
+            // cboCustomer
+            // 
+            this.cboCustomer.FormattingEnabled = true;
+            this.cboCustomer.Location = new System.Drawing.Point(167, 30);
+            this.cboCustomer.Name = "cboCustomer";
+            this.cboCustomer.Size = new System.Drawing.Size(121, 21);
+            this.cboCustomer.TabIndex = 4;
+            this.cboCustomer.SelectedIndexChanged += new System.EventHandler(this.cboCustomer_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Style";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(164, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Customer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboCustomer);
+            this.Controls.Add(this.cboStyle);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.dgvColumns);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Costing";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvColumns)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvColumns;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Style;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Customer;
+        private System.Windows.Forms.DataGridViewTextBoxColumn F1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn F2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn F3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn F4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn F5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn F6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn F7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn F8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn F9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn F10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn F11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn F12;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ComboBox cboStyle;
+        private System.Windows.Forms.ComboBox cboCustomer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Costing
+{
+    public partial class Form1 : Form
+    {
+        private const string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=dbdemo;Integrated Security=True";
+        private string selectedStyle;
+        private string selectedCustomer;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            PopulateComboBox();
+            // Call method to populate ComboBox when the form loads
+
+
+            //cboStyle.SelectedIndexChanged += cboStyle_SelectedIndexChanged; // Subscribe to SelectedIndexChanged event
+            //cboCustomer.SelectedIndexChanged += cboCustomer_SelectedIndexChanged; // Subscribe to SelectedIndexChanged even
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //object[] rowData1 = new object[] { " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " " };
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            dgvColumns.Rows.Add(selectedStyle, selectedCustomer, "DATED", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ");
+            dgvColumns.Rows.Add(selectedStyle, selectedCustomer, "STYLE", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ");
+            dgvColumns.Rows.Add(selectedStyle, selectedCustomer, "CUSTOMER BRAND", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ");
+            dgvColumns.Rows.Add(selectedStyle, selectedCustomer, "COUNTRY OF ORIGIN", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ");
+            dgvColumns.Rows.Add(selectedStyle, selectedCustomer, "FACTORY", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ");
+            dgvColumns.Rows.Add(selectedStyle, selectedCustomer, "DESCRIPTION", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ");
+            dgvColumns.Rows.Add(selectedStyle, selectedCustomer, "SEASON", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ");
+
+            DataGridViewRow fabricRow1 = new DataGridViewRow();
+            fabricRow1.CreateCells(dgvColumns, selectedStyle, selectedCustomer, "FABRIC", "CODE", "", "", "", "", "", "", "", "", "UNIT", "CIF BKK", "CONSUMPTION/PC", "AMOUNT/USD");
+
+            // Set all cells in fabricRow1 as read-only
+            for (int i = 0; i < fabricRow1.Cells.Count; i++)
+            {
+                fabricRow1.Cells[i].ReadOnly = true;
+            }
+
+            // Add fabricRow1 to the DataGridView
+            dgvColumns.Rows.Add(fabricRow1);
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Create a SQL command to retrieve the maximum RowValue from the database
+                string selectMaxRowQuery = "SELECT ISNULL(MAX([Row]), 0) FROM TestTableExcel;";
+
+                SqlCommand selectMaxRowCommand = new SqlCommand(selectMaxRowQuery, connection);
+
+                // Get the maximum RowValue from the database
+                int maxRowValue = Convert.ToInt32(selectMaxRowCommand.ExecuteScalar());
+
+                // Set the new RowValue to be one greater than the maximum RowValue in the database
+                int newRowValue = maxRowValue + 1;
+
+                // Create a SQL command to insert data
+                string insertQuery = @"
+            INSERT INTO TestTableExcel ([Row], Style, Customer, F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12)
+            VALUES (@RowValue, @Style, @Customer, @F1, @F2, @F3, @F4, @F5, @F6, @F7, @F8, @F9, @F10, @F11, @F12);";
+
+                SqlCommand insertCommand = new SqlCommand(insertQuery, connection);
+
+                // Add parameters to the insert command
+                insertCommand.Parameters.Add("@RowValue", SqlDbType.Int).Value = newRowValue;
+                insertCommand.Parameters.Add("@Style", SqlDbType.VarChar);
+                insertCommand.Parameters.Add("@Customer", SqlDbType.VarChar);
+                insertCommand.Parameters.Add("@F1", SqlDbType.VarChar);
+                insertCommand.Parameters.Add("@F2", SqlDbType.VarChar);
+                insertCommand.Parameters.Add("@F3", SqlDbType.VarChar);
+                insertCommand.Parameters.Add("@F4", SqlDbType.VarChar);
+                insertCommand.Parameters.Add("@F5", SqlDbType.VarChar);
+                insertCommand.Parameters.Add("@F6", SqlDbType.VarChar);
+                insertCommand.Parameters.Add("@F7", SqlDbType.VarChar);
+                insertCommand.Parameters.Add("@F8", SqlDbType.VarChar);
+                insertCommand.Parameters.Add("@F9", SqlDbType.VarChar);
+                insertCommand.Parameters.Add("@F10", SqlDbType.VarChar);
+                insertCommand.Parameters.Add("@F11", SqlDbType.VarChar);
+                insertCommand.Parameters.Add("@F12", SqlDbType.VarChar);
+
+                // Track if any insert operation failed
+                bool success = true;
+
+                // Iterate through DataGridView rows and execute the insert command for each row
+                foreach (DataGridViewRow row in dgvColumns.Rows)
+                {
+                    // Check if the cell's value is not null before accessing it
+                    string style = row.Cells[0].Value?.ToString() ?? "";
+                    string customer = row.Cells[1].Value?.ToString() ?? "";
+                    string f1 = row.Cells[2].Value?.ToString() ?? "";
+                    string f2 = row.Cells[3].Value?.ToString() ?? "";
+                    string f3 = row.Cells[4].Value?.ToString() ?? "";
+                    string f4 = row.Cells[5].Value?.ToString() ?? "";
+                    string f5 = row.Cells[6].Value?.ToString() ?? "";
+                    string f6 = row.Cells[7].Value?.ToString() ?? "";
+                    string f7 = row.Cells[8].Value?.ToString() ?? "";
+                    string f8 = row.Cells[9].Value?.ToString() ?? "";
+                    string f9 = row.Cells[10].Value?.ToString() ?? "";
+                    string f10 = row.Cells[11].Value?.ToString() ?? "";
+                    string f11 = row.Cells[12].Value?.ToString() ?? "";
+                    string f12 = row.Cells[13].Value?.ToString() ?? "";
+
+                    // Set parameter values
+                    insertCommand.Parameters["@Style"].Value = style;
+                    insertCommand.Parameters["@Customer"].Value = customer;
+                    insertCommand.Parameters["@F1"].Value = f1;
+                    insertCommand.Parameters["@F2"].Value = f2;
+                    insertCommand.Parameters["@F3"].Value = f3;
+                    insertCommand.Parameters["@F4"].Value = f4;
+                    insertCommand.Parameters["@F5"].Value = f5;
+                    insertCommand.Parameters["@F6"].Value = f6;
+                    insertCommand.Parameters["@F7"].Value = f7;
+                    insertCommand.Parameters["@F8"].Value = f8;
+                    insertCommand.Parameters["@F9"].Value = f9;
+                    insertCommand.Parameters["@F10"].Value = f10;
+                    insertCommand.Parameters["@F11"].Value = f11;
+                    insertCommand.Parameters["@F12"].Value = f12;
+
+                    // Execute the command
+                    try
+                    {
+                        insertCommand.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        // If an error occurs during insertion, set success to false and break the loop
+                        success = false;
+                        MessageBox.Show("Error: " + ex.Message);
+                        break;
+                    }
+                }
+
+                // Check if all insertions were successful
+                if (success)
+                {
+                    MessageBox.Show("All data inserted successfully.");
+                }
+                else
+                {
+                    MessageBox.Show("Some data could not be inserted. Please check and try again.");
+                }
+            }
+        }
+
+        private void cboStyle_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //string selectedValue = cboStyle.SelectedItem.ToString();
+            selectedStyle = cboStyle.SelectedItem.ToString();
+            //MessageBox.Show("Selected Item: " + selectedStyle);
+        }
+
+        private void cboCustomer_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedCustomer = cboCustomer.SelectedItem.ToString();
+            //MessageBox.Show("Selected Item: " + selectedCustomer);
+        }
+
+
+        private void PopulateComboBox()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string query = "SELECT * FROM CUSTOMER";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    cboStyle.Items.Clear();
+                    while (reader.Read())
+                    {
+                        cboStyle.Items.Add(reader.GetString(2));
+                    }
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string query = "SELECT * FROM AGENTS";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    cboCustomer.Items.Clear();
+                    while (reader.Read())
+                    {
+                        cboCustomer.Items.Add(reader.GetString(1));
+                    }
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+
+    }
+}
